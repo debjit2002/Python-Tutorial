89702c3d255c192c53dd2e471934d10eb25220a0
Debjit <debjitacharjee07@gmail.com>
2025-06-07 22:59:44 +0530

    Type Casting

---
+++ 9_TypeCasting.py
@@ -0,0 +1,14 @@
+a = "1"
+b = "2"
+c = 3.9
+d = 4
+print(a+b)
+
+#Explicit Type Casting -> Conversion of one data type to another done via developer or programmer's intervention 
+print(int(a)+int(b))
+
+#Implicit Type Casting -> There are data types which are higher as well as lower in order . Python converts smaller data type to higher data type . 
+# One data type is converted into other by the Python interpreter automatially . It prevents data loss . 
+print(c+d)
+print(type(c+d))
+

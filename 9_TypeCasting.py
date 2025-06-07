6570745c3c99695cae8c84f8a59370596c6b0214
Debjit <debjitacharjee07@gmail.com>
2025-06-07 23:16:07 +0530

    NEW ADDED LINE

--- 9_TypeCasting.py
+++ 9_TypeCasting.py
@@ -2,8 +2,10 @@ a = "1"
 b = "2"
 c = 3.9
 d = 4
+e = 5.5
+f = 6 
 print(a+b)
-
+print(e+f)
 #Explicit Type Casting -> Conversion of one data type to another done via developer or programmer's intervention 
 print(int(a)+int(b))
 

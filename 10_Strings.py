c2373e4a621c38bc3dabeb402e0616565ca033c0
Debjit <debjitacharjee07@gmail.com>
2025-06-08 00:41:05 +0530

    ADDED MULTILINE CONCEPT

--- 10_Strings.py
+++ 10_Strings.py
@@ -12,5 +12,14 @@ for character in name: #Accessing each character using for loop
     
 sent = "Hi Harry,\"How are you\"?" #If we want to use quotation marks in between a sentence, we use backslash("\") to highlight it otherwise it might show you an error.
 print(sent)
+
+#Printing multiline sentences
+multi = '''Save Water: "Water is a precious resource that is essential for life. Conserving water is crucial for maintaining our environment and ensuring that future generations have access to clean water. 
+Simple actions like fixing leaks, using water-efficient appliances, and collecting rainwater can significantly reduce water waste." 
+
+The Importance of Healthy Eating: "Healthy eating is vital for maintaining overall health and well-being. A balanced diet rich in fruits, vegetables, whole grains, and lean proteins can help prevent chronic diseases and promote a healthy weight. 
+Making informed food choices and being mindful of portion sizes are key components of a healthy lifestyle."'''
+
+print(multi)
     
 

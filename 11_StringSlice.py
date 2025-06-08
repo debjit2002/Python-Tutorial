4129d1dc110b8071639ff60a4a43bc4b33df9896
Debjit <debjitacharjee07@gmail.com>
2025-06-08 23:40:01 +0530

    ADDED STRING SLICING CODE

---
+++ 11_StringSlice.py
@@ -0,0 +1,15 @@
+#-------------------STRING SLICING-----------------------------
+fruit = "Mango"
+print(fruit[0:5]) #O/P -> Mango
+
+#Negative String Slicing -> Try to calculate negative index by deducting from length of str.
+#Here in this case,we are calculating fruit(names[0:-3]) as fruit(names[0:len(fruit)-3]) -> (fruit[0:2])
+print(fruit[0:-3]) #O/P -> Ma
+
+print(fruit[-3:-2]) #O/P -> n
+print(fruit[:]) #O/P -> Mango
+
+
+names ="John,Mary"
+print(len(names)) #Calculate length of string
+print(names[0:2])
